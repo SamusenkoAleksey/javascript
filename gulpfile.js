d89144d1d8f3c71bca861080a58f580096ab3463
Aleksey Samusenko <aleksej.samusenko93@gmail.com>
2016-04-22 23:42:38 +0300

    another commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-var gulp = require('gulp'),
-	concatCss = require('gulp-concat-css');
+// var gulp = require('gulp'),
+// 	concatCss = require('gulp-concat-css');
 
-gulp.task('test', function () {
+// gulp.task('test', function () {
+//   return gulp.src('./public/css/*.css')
+//     .pipe(concatCss("bundle.css"))
+//     .pipe(gulp.dest('./build/'));
+// });
+
+var gulp = require('gulp'),
+	concatCss = require('gulp-concat-css'),
+	cssmin = require('gulp-cssmin'),
+	rename = require('gulp-rename');
+
+gulp.task('css', function () {
   return gulp.src('./public/css/*.css')
     .pipe(concatCss("bundle.css"))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./build/'));
 });
