4f223352d550aa8536d0292bc85611785c9393cf
Aleksey Samusenko <aleksej.samusenko93@gmail.com>
2016-04-23 12:14:19 +0300

    adding gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,8 @@
-// var gulp = require('gulp'),
-// 	concatCss = require('gulp-concat-css');
-
-// gulp.task('test', function () {
-//   return gulp.src('./public/css/*.css')
-//     .pipe(concatCss("bundle.css"))
-//     .pipe(gulp.dest('./build/'));
-// });
-
 var gulp = require('gulp'),
-	concatCss = require('gulp-concat-css'),
-	cssmin = require('gulp-cssmin'),
-	rename = require('gulp-rename');
+	concatCss = require('gulp-concat-css');
 
-gulp.task('css', function () {
+gulp.task('concat', function () {
   return gulp.src('./public/css/*.css')
     .pipe(concatCss("bundle.css"))
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./build/'));
 });
