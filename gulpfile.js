5197faadad009615850839de35fcc915ce258f44
Aleksey Samusenko <aleksej.samusenko93@gmail.com>
2016-04-23 22:50:58 +0300

    using gulp css minimisator

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp'),
-	concatCss = require('gulp-concat-css');
+	concatCss = require('gulp-concat-css'),
+	cssmin = require('gulp-cssmin'),
+	rename = require('gulp-rename');
 
-gulp.task('concat', function () {
+gulp.task('css', function () {
   return gulp.src('./public/css/*.css')
     .pipe(concatCss("bundle.css"))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./build/'));
 });
